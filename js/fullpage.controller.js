820b6da0daa6e14be226abea60049d6e0ebd2d48
Lasantha Premarathna <lasanthpremarathna@gmail.com>
2018-06-02 23:49:32 +0530

    Initial Commit

---
+++ js/fullpage.controller.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        sectionsColor: ['#F74534', '#1A86EA', '#F35269', '#FAB700'],
+        navigation: true,
+        navigationTooltips: ['Home', 'About Us', 'Portfolio', 'Contact Us'],
+        responsiveHeight: 500
+    });
+});
