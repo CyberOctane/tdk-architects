b344d944c021760aac9e66050cc81ce84b53b2b0
Lasantha Premarathna <lasanthpremarathna@gmail.com>
2018-06-16 02:52:36 +0530

    Section 01 Updated

--- js/fullpage.controller.js
+++ js/fullpage.controller.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        sectionsColor: ['#F74534', '#1A86EA', '#F35269', '#FAB700'],
+        sectionsColor: ['#FFFFFF', '#1A86EA', '#F35269', '#FAB700', '#F74534'],
         navigation: true,
         navigationTooltips: ['Home', 'About Us', 'Portfolio', 'Contact Us'],
         responsiveHeight: 500
